75024d12dfb547ccb8b1bb45446d18bc2fbcd56d
Meghanabojja <124967219+Meghanabojja@users.noreply.github.com>
2023-04-12 22:19:08 -0400

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,13 @@
+#ifndef __FUNCTIONS__
+#define __FUNCTIONS__
 
+typedef struct serialized_buffer{
+    #define BUFFER_DEFAULT_SIZE 512
+    void *b;
+    int size;
+    int next;
+} ser_buff_t;
+
+void serialize_data(ser_buff_t *b,  const char *data, int val_size);
+
+#endif
