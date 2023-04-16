5f49994f7c69d4db75d7407efc3da05144e5af25
Deshitha23 <125763130+Deshitha23@users.noreply.github.com>
2023-04-16 15:36:54 -0400

    Create server.h

---
+++ server.h
@@ -0,0 +1,14 @@
+#ifndef __RPC_COMMON__
+#define __RPC_COMMON__
+
+#define MAX_RECV_SEND_BUFF_SIZE 2048
+
+
+
+
+
+#define SERVER_PORT 4567
+#define SERVER_IP "172.19.50.191"
+//server_IP address should be your server's IP address when you want to connect through different machines
+
+#endif /* __RPC_COMMON__ */
