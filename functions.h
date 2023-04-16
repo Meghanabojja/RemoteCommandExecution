a341fab9467c6d9b61e2e32de62c4b2c5f1b5e43
Meghanabojja <124967219+Meghanabojja@users.noreply.github.com>
2023-04-16 15:29:18 -0400

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,17 +1,44 @@
 #ifndef __FUNCTIONS__
 #define __FUNCTIONS__
 
+/* Data types supported*/
+
 typedef struct serialized_buffer{
     #define BUFFER_DEFAULT_SIZE 512
     void *b;
     int size;
     int next;
-} ser_buff_t;
+} buffer_t;
 
-void serialize_data(ser_buff_t *b,  const char *data, int val_size);
-void init_serialized_buffer(ser_buff_t **b);
+/* init functions*/
+void init_buffer(buffer_t **b);
+void init_buffer_of_size(buffer_t **b, int size);
 
-int is_serialized_buffer_empty(const ser_buff_t *b)
-void free_serialized_buffer(ser_buff_t *b)
-    
+/* Serialize functions*/
+
+char buffer_empty(buffer_t *b);
+
+/* get functions*/
+int  get_buffer_datasize(buffer_t *b);
+int get_buffer_length(buffer_t *b);
+int get_buffer_current_pointer_offset(buffer_t *b);
+char *get_buffer_current_pointer(buffer_t *b);
+void buffer_skip(buffer_t *b, int size);
+void copy_in_buffer_by_offset(buffer_t *b, int size, char *value, int offset);
+
+/* De-Serialize function */
+
+void serialize(buffer_t *b, char *data, int val_size);
+void deserialize(char *dest, buffer_t *b, int val_size);
+
+/* free Resourse*/
+void free_buffer(buffer_t *b);
+
+/reset function/
+void truncate_buffer(buffer_t **b);
+void reset_buffer(buffer_t *b);
+/* Details*/
+void buffer_details(buffer_t *b, const char *fn, int lineno);
+void  reset_serialize_buffer(buffer_t *b);
+
 #endif
