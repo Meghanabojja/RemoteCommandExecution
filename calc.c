e2c1f8f6acd6a8103f800b9087062392cd089538
PunithKandula <114423814+punithkandula@users.noreply.github.com>
2023-04-16 16:56:39 -0400

    Update calc.c

--- calc.c
+++ calc.c
@@ -12,17 +12,23 @@ int main(int argc, char **argv){
     int x,a, b;
     printf("Enter your choice\n1.Addition\n2.subraction\n3.multiply\n4.Division\n");
     scanf("%d",&x);
-    
-    printf("Enter Ist number : ");
-    scanf("%d", &a);
-    printf("Enter IInd number : ");
-    scanf("%d", &b);
+   if(x>=1 && x<=4) 
+   {
+       printf("Enter Ist number : ");
+        scanf("%d", &a);
+        printf("Enter IInd number : ");
+        scanf("%d", &b);
 
     /*Invoke the rpc*/
     int res = multiply_rpc(x,a, b);
 
     /*Print the result*/
     printf("res = %d\n", res);
+   }
+   else
+   {
+       printf("Invalid Choice");
+   }
     return 0; 
 
     
